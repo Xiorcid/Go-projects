edd5aad2e2a6030412d7db75627150712c66efa7
Xiorcid <petuh0156@gmail.com>
2024-09-24 09:15:50 +0300

    Homework 1

--- Homework 1/length/length.go
+++ Homework 1/length/length.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	var a, b string
+	var a, b float32
 
 	fmt.Println("Feets and inches converter")
 	fmt.Println()
 
-	fmt.Print("Enter length (3ft 5in): ")
+	fmt.Print("Enter feets: ")
 	fmt.Scan(&a)
+	fmt.Println()
+
+	fmt.Print("Enter inches: ")
 	fmt.Scan(&b)
 	fmt.Println()
 
-	ft, err := strconv.Atoi(strings.Split(a, "ft")[0])
-	in, err := strconv.Atoi(strings.Split(b, "in")[0])
-	if err == nil {
-		fmt.Printf("Length: %fcm", float32(ft)*30.48+float32(in)*2.54)
-	}
+	fmt.Printf("Length: %fcm", a*30.48+b*2.54)
 }
